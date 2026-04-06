3578b8016af461fd018bcb4e5e283411f246fd1d
Aayush Kumar <aayushkumar9376@gmail.com>
2026-04-07 03:58:27 +0530

    added error handling middleware

--- backend/index.js
+++ backend/index.js
@@ -2,25 +2,43 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+
 import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-const app = express();
-
-await connectDB();
-
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => res.send("Server is Live!"));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to connect DB:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
